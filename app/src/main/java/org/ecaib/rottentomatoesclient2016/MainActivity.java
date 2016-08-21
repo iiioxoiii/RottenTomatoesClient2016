98454833d2789758982335babfd252ce475594dc
lawer <carlesgm@gmail.com>
2016-08-21 22:42:53 +0200

    Obrim el SettingsActivity quan es seleccione l'opció corresponent.

--- app/src/main/java/org/ecaib/rottentomatoesclient2016/MainActivity.java
+++ app/src/main/java/org/ecaib/rottentomatoesclient2016/MainActivity.java
@@ -1,5 +1,6 @@
 package org.ecaib.rottentomatoesclient2016;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -35,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent i = new Intent(this, SettingsActivity.class);
+            startActivity(i);
             return true;
         }
 
