c292255fcd1ab10d8e33d69d570160562f813f99
lawer <carlesgm@gmail.com>
2016-08-21 19:48:22 +0200

    - Agreguem un listview anomenat lvPelis. - L'enllacem desde el fragment.

--- app/src/main/java/org/ecaib/rottentomatoesclient2016/MainActivityFragment.java
+++ app/src/main/java/org/ecaib/rottentomatoesclient2016/MainActivityFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +18,10 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView lvPelis = (ListView) view.findViewById(R.id.lvPelis);
+
+        return view;
     }
 }
