d8df0a1328ab5524fada5aecf46d837e0dcab219
lawer <carlesgm@gmail.com>
2016-08-21 20:04:05 +0200

    Gestionem el clicks en "Refresh"

--- app/src/main/java/org/ecaib/rottentomatoesclient2016/MainActivityFragment.java
+++ app/src/main/java/org/ecaib/rottentomatoesclient2016/MainActivityFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -65,5 +66,25 @@ public class MainActivityFragment extends Fragment {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.menu_pelis_fragment, menu);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_refresh) {
+            refresh();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void refresh() {
+    }
+
 
 }
