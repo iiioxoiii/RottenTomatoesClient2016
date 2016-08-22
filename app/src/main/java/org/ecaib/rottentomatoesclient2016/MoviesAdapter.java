be41e980eab7d2727934874a2d1865781c5d997e
lawer <carlesgm@gmail.com>
2016-08-22 17:03:58 +0200

    Fem el mateix en l'adapter

--- app/src/main/java/org/ecaib/rottentomatoesclient2016/MoviesAdapter.java
+++ app/src/main/java/org/ecaib/rottentomatoesclient2016/MoviesAdapter.java
@@ -1,15 +1,16 @@
 package org.ecaib.rottentomatoesclient2016;
 
 import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+
+import org.ecaib.rottentomatoesclient2016.databinding.LvPelisRowBinding;
 
 import java.util.List;
 
@@ -27,25 +28,24 @@ public class MoviesAdapter extends ArrayAdapter<Movie> {
         Movie movie = getItem(position);
         Log.w("XXXX", movie.toString());
 
+        LvPelisRowBinding binding = null;
+
         // Mirem a veure si la View s'està reusant, si no es així "inflem" la View
         // https://github.com/codepath/android_guides/wiki/Using-an-ArrayAdapter-with-ListView#row-view-recycling
         if (convertView == null) {
             LayoutInflater inflater = LayoutInflater.from(getContext());
-            convertView = inflater.inflate(R.layout.lv_pelis_row, parent, false);
+            binding = DataBindingUtil.inflate(inflater, R.layout.lv_pelis_row, parent, false);
+        } else {
+            binding = DataBindingUtil.getBinding(convertView);
         }
 
-        // Unim el codi en les Views del Layout
-        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
-        TextView tvCriticsScore = (TextView) convertView.findViewById(R.id.tvCriticsScore);
-        ImageView ivPosterImage = (ImageView) convertView.findViewById(R.id.ivPosterImage);
-
         // Fiquem les dades dels objectes (provinents del JSON) en el layout
-        tvTitle.setText(movie.getTitle());
-        tvCriticsScore.setText("Score: " + movie.getCritics_score() + "%");
-        Glide.with(getContext()).load(movie.getPosterUrl()).into(ivPosterImage);
+        binding.tvTitle.setText(movie.getTitle());
+        binding.tvCriticsScore.setText("Score: " + movie.getCritics_score() + "%");
+        Glide.with(getContext()).load(movie.getPosterUrl()).into(binding.ivPosterImage);
 
         // Retornem la View replena per a mostrarla
-        return convertView;
+        return binding.getRoot();
     }
 
 }
