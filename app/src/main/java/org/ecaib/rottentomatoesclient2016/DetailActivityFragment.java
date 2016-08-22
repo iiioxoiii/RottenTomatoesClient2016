f511c9778b59ca00f29d5138da0092db71c3571d
lawer <carlesgm@gmail.com>
2016-08-22 10:50:18 +0200

    Agafem les dades en el Fragment de detal i les mostrem

--- app/src/main/java/org/ecaib/rottentomatoesclient2016/DetailActivityFragment.java
+++ app/src/main/java/org/ecaib/rottentomatoesclient2016/DetailActivityFragment.java
@@ -1,7 +1,9 @@
 package org.ecaib.rottentomatoesclient2016;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +19,22 @@ public class DetailActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        Intent i = getActivity().getIntent();
+
+        if (i != null) {
+            Movie movie = (Movie) i.getSerializableExtra("movie");
+
+            if (movie != null) {
+                updateUi(movie);
+            }
+        }
+
+        return view;
+    }
+
+    private void updateUi(Movie movie) {
+        Log.d("MOVIE", movie.toString());
     }
 }
