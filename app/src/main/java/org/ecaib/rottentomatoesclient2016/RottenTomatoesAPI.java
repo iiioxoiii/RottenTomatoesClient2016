5e32baed6a53618e148fa18e6cc5aa74e0201579
Carles Gonzalez <carlesgm@gmail.com>
2017-08-27 20:04:38 +0200

    Nou métode per a obtindre les pròximes estrenes

--- app/src/main/java/org/ecaib/rottentomatoesclient2016/RottenTomatoesAPI.java
+++ app/src/main/java/org/ecaib/rottentomatoesclient2016/RottenTomatoesAPI.java
@@ -26,4 +26,23 @@ public class RottenTomatoesAPI {
         }
         return null;
     }
+
+    String getProximesEstrenes(String pais) {
+        Uri builtUri = Uri.parse(BASE_URL)
+                .buildUpon()
+                .appendPath("movie")
+                .appendPath("upcoming")
+                .appendQueryParameter("region", pais)
+                .appendQueryParameter("api_key", API_KEY)
+                .build();
+        String url = builtUri.toString();
+
+        try {
+            String JsonResponse = HttpUtils.get(url);
+            return JsonResponse;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
