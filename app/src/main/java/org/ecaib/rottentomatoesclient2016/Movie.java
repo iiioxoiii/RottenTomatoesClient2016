8e397070e57a68b9709f99cdfa1ef20a6fb25139
Carles Gonzalez <carlesgm@gmail.com>
2017-08-27 20:40:59 +0200

    Actualitzem la classe movie

--- app/src/main/java/org/ecaib/rottentomatoesclient2016/Movie.java
+++ app/src/main/java/org/ecaib/rottentomatoesclient2016/Movie.java
@@ -1,16 +1,10 @@
 package org.ecaib.rottentomatoesclient2016;
 
-import android.text.TextUtils;
-
-import java.util.ArrayList;
-
 public class Movie {
     private String title;
-    private int year;
-    private String synopsis;
-    private String posterUrl;
-    private int criticsScore;
-    private ArrayList<String> castList;
+    private String releaseDate;
+    private String overview;
+    private String posterPath;
 
     public String getTitle() {
         return title;
@@ -20,55 +14,37 @@ public class Movie {
         this.title = title;
     }
 
-    public int getYear() {
-        return year;
+    public String getReleaseDate() {
+        return releaseDate;
     }
 
-    public void setYear(int year) {
-        this.year = year;
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
     }
 
-    public String getSynopsis() {
-        return synopsis;
+    public String getOverview() {
+        return overview;
     }
 
-    public void setSynopsis(String synopsis) {
-        this.synopsis = synopsis;
+    public void setOverview(String overview) {
+        this.overview = overview;
     }
 
-    public String getPosterUrl() {
-        return posterUrl;
+    public String getPosterPath() {
+        return posterPath;
     }
 
-    public void setPosterUrl(String posterUrl) {
-        this.posterUrl = posterUrl;
-    }
-
-    public int getCriticsScore() {
-        return criticsScore;
-    }
-
-    public void setCriticsScore(int criticsScore) {
-        this.criticsScore = criticsScore;
-    }
-
-    public ArrayList<String> getCastList() {
-        return castList;
-    }
-
-    public void setCastList(ArrayList<String> castList) {
-        this.castList = castList;
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
     }
 
     @Override
     public String toString() {
         return "Movie{" +
                 "title='" + title + '\'' +
-                ", year=" + year +
-                ", synopsis='" + synopsis + '\'' +
-                ", posterUrl='" + posterUrl + '\'' +
-                ", criticsScore=" + criticsScore +
-                ", castList=" + castList +
+                ", releaseDate='" + releaseDate + '\'' +
+                ", overview='" + overview + '\'' +
+                ", posterPath='" + posterPath + '\'' +
                 '}';
     }
 }
